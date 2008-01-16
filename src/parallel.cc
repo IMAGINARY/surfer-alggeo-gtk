fc29640a8ea24033f29e2f0440647ca2acc21216
henningmeyer <hmeyer@mathematik.uni-kl.de>
2008-01-16 16:46:54 +0000

    singlecore works with unmodified surf
    git-svn-id: svn+ssh://hausach.mfo.de/localhome/imaginar/svn/imaginary/surfer/trunk@31 4beaa02c-d2ff-4d45-9dad-6a4a666722bb

--- src/parallel.cc
+++ src/parallel.cc
@@ -79,7 +79,9 @@ void parallel_surf(const std::string& script,bool sync, int width, const surfer_
 		if(i)
 		buf<<(i+1);
 
-		std::string cmd = (opt.surf_cmd + (" --clip_to 0 "+min_buff.str()+" "+fbuff+" "+" "+max_buff.str())+" "SURF_OPT +" \""+script+buf.str()+"\" " REDIRECTION_APEX +((!sync) ?"":DAEMONIZE));
+		std::string cmd = (opt.surf_cmd + ((n!=1)?(" --clip_to 0 "+min_buff.str()+" "+fbuff+" "+" "+max_buff.str()):"")+" "SURF_OPT +" \""+script+buf.str()+"\" " REDIRECTION_APEX +((!sync) ?"":DAEMONIZE));
+
+
 		//std::cout<<cmd<<std::endl;
 		system(cmd.c_str());
 	}
