93004f711be454e38fc375014a7379306295e2a9
henningmeyer <hmeyer@mathematik.uni-kl.de>
2008-03-20 14:30:26 +0000

    removed unfinished experimental code
    git-svn-id: svn+ssh://hausach.mfo.de/localhome/imaginar/svn/imaginary/surfer/trunk@112 4beaa02c-d2ff-4d45-9dad-6a4a666722bb

--- src/win32.cc
+++ src/win32.cc
@@ -85,66 +85,6 @@ std::cout<<"HOME "<<bu<<std::endl;
 
 
 
-class AniDia: public Gtk::Dialog
-{
-
-};
-
-    /**
-     * Sets the value of this quaternion to the rotational component of
-     * the passed matrix.
-     * @param m1 the matrix3f
-     */
-matrix<double>    rot2quat(const matrix<double>& m1)
-    {
-	matrix<double> quat(4,1);
-
-	double ww = 0.25*(m1.el(1,1) + m1.el(2,2) + m1.el(3,3) + 1.0);
-
-	if (ww >= 0) {
-	    if (ww >= EPS2) {
-		quat.el(1,1) = Math.sqrt(ww);
-		ww = 0.25/quat.el(1,1);
-		quat.el(2,1) = ((m1.m21 - m1.m12)*ww);
-		quat.el(3,1) = ((m1.m02 - m1.m20)*ww);
-		quat.el(4,1) = ((m1.m10 - m1.m01)*ww);
-		return;
-	    }
-	} else {
-	    this.w = 0;
-	    this.x = 0;
-	    this.y = 0;
-	    this.z = 1;
-	    return;
-	}
-
-	this.w = 0;
-	ww = -0.5*(m1.m11 + m1.m22);
-	if (ww >= 0) {
-	    if (ww >= EPS2) {
-		this.x = Math.sqrt(ww);
-		ww = 0.5/this.x;
-		this.y = (m1.m10*ww);
-		this.z = (m1.m20*ww);
-		return;
-	    }
-	} else {
-	    this.x = 0;
-	    this.y = 0;
-	    this.z = 1;
-	    return;
-	}
-     
-	this.x = 0;
-	ww =  0.5*(1.0 - m1.m22);
-	if (ww >= EPS2) {
-	    this.y = Math.sqrt(ww);
-	    this.z = (m1.m21/(2.0*this.y));
-	}
-     
-	this.y =  0;
-	this.z =  1;
-    }
 
 
 
