f58a74869c2b87e71d53df49a93d90c9e7bdae84
porst17 <stussak@mfo.de>
2009-09-15 10:40:34 +0000

    cleaned up printing script, removed name prompt when printing
    git-svn-id: svn+ssh://hausach.mfo.de/localhome/imaginar/svn/imaginary/surfer/trunk@203 4beaa02c-d2ff-4d45-9dad-6a4a666722bb

--- linux-src/printing/print.sh
+++ linux-src/printing/print.sh
@@ -1,51 +1,36 @@
 #!/bin/sh
 
-# $1 ist die Datei mit dem Bild
-# $2 ist die Datei mit der Formel
-# $3 ist die Datei mit dem Namen
+echo first $1
+echo second $2
 
-HERE="." # hier muss der Ordner angegeben werden, der die .tex-Dateien und dies Skript enthaelt
+# enter directory path to this shell script
+HERE="/home/mima/Desktop/IMAGINARY/surfer_printing"
 IMAGE_FILE=$HERE/image.png
 FORMULA_FILE=$HERE/formula.tex
-LATEX_FILE_N_NAME=$HERE/surfer_print
-LATEX_FILE_W_NAME=$HERE/surfer_with_name
-NAME_FILE=$HERE/name.tex
+LATEX_FILE=$HERE/surfer_print
 
 FORMULA_EPS_TEX=$HERE/surfer_formula
 
-echo $1
-echo $2
-echo $3
-
-echo printing
-
-if ! test -e $FORMULA_FILE ; then
-echo bitte HERE= im Skript aendern; fi
-
 cp $1 $IMAGE_FILE
 cat $2 | $HERE/texify.pl > $FORMULA_FILE
-if test -e $3; then cp $3 $NAME_FILE; fi
+#cat $PREMATH $FORMULA_FILE.pre $POSTMATH > $FORMULA_FILE
 
 cd $HERE
 convert image.png image.eps
 
-latex -interaction=nonstopmode $FORMULA_EPS_TEX.tex
+latex -interactive=nonstopmode $FORMULA_EPS_TEX.tex
 dvips -E $FORMULA_EPS_TEX.dvi
 eps2eps $FORMULA_EPS_TEX.ps eps_formula.eps
 
-if test -e $3 ;  then
-LATEX_FILE=$LATEX_FILE_W_NAME
-else
-LATEX_FILE=$LATEX_FILE_N_NAME
-fi
-
 latex -interaction=nonstopmode $LATEX_FILE.tex
 dvips -t a5 $LATEX_FILE.dvi
-lpr   $LATEX_FILE.ps;
+lpr   $LATEX_FILE.ps
+ps2pdf $LATEX_FILE.ps
 
+# create archive copy
 mkdir -p archive
 timestamp=`date +%Y-%m-%d_%H-%M-%S`
-cp $LATEX_FILE.ps archive/$timestamp.ps
+cp $LATEX_FILE.pdf archive/$timestamp.pdf
 cp $1 archive/$timestamp.png
-cp /tmp/$USER""surfb_f_p.pic archive/$timestamp.pic
+cp /tmp/usersurfb_f_p.pic archive/$timestamp.pic
 
