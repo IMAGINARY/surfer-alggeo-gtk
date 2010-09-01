ba765bae55e0fea374785b1d153ad5fbb8f62078
porst17 <stussak@mfo.de>
2010-09-01 21:06:09 +0000

    creates script that compiles surfer and surf and that creates a debian package
    git-svn-id: svn+ssh://hausach.mfo.de/localhome/imaginar/svn/imaginary/surfer/trunk@250 4beaa02c-d2ff-4d45-9dad-6a4a666722bb

--- create_deb.sh
+++ create_deb.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 echo Installs surfer and creates a .deb from the installation
+# build surf
+cd surf
+sudo apt-get install build-essential g++ flex bison zlib1g-dev libjpeg-dev libtiff-dev libgmp3-dev libx11-dev libgtkmm-2.4-dev
+chmod +x ./configure ./missing
+CFLAGS="-O3 -fopenmp" CXXFLAGS="-O3 -fopenmp" ./configure --disable-gui
+make
+cd ..
+
+# build and install surfer
+chmod +x ./configure ./missing
+CFLAGS="-O3 -fopenmp" CXXFLAGS="-O3 -fopenmp" ./configure
 sudo make install
 DEB=./deb
 rm -rf $DEB/usr/local
 mkdir -p $DEB/usr/local/bin/
 mkdir -p $DEB/usr/local/share/surf/
 mkdir -p $DEB/usr/local/share/surfer/ 
-cp /usr/local/bin/surf $DEB/usr/local/bin/
+cp ./surf/surf $DEB/usr/local/bin/
 cp /usr/local/bin/surfer $DEB/usr/local/bin/
-cp -r /usr/local/share/surf/ $DEB/usr/local/share/surf/
 cp -r /usr/local/share/surfer/ $DEB/usr/local/share/surfer/ 
 rm -f $DEB/usr/local/share/surfer/gallery/gallery.tar
 cp surfer_ui_exhibition.xml $DEB/usr/local/share/surfer/
