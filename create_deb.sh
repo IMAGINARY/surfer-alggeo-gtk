246e7302d267c9fcba976869f7595f07f85ab772
porst17 <stussak@mfo.de>
2010-11-04 13:40:15 +0000

    restructured script for creating of Debian package included locale files in Debian package
    git-svn-id: svn+ssh://hausach.mfo.de/localhome/imaginar/svn/imaginary/surfer/trunk@260 4beaa02c-d2ff-4d45-9dad-6a4a666722bb

--- create_deb.sh
+++ create_deb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo Installs surfer and creates a .deb from the installation
-# build surf
+echo -e "\033[1mInstalls surfer and creates a .deb from the installation" && tput sgr0
+echo -e "\033[1mbuilding surf" && tput sgr0
 cd surf
 sudo apt-get install build-essential g++ flex bison zlib1g-dev libjpeg-dev libtiff-dev libgmp3-dev libx11-dev libgtkmm-2.4-dev
 chmod +x ./configure ./missing
@@ -8,18 +8,45 @@ CFLAGS="-O3 -fopenmp" CXXFLAGS="-O3 -fopenmp" ./configure --disable-gui
 make
 cd ..
 
-# build and install surfer
+echo -e "\033[1mbuilding and installing surfer" && tput sgr0
 chmod +x ./configure ./missing
 CFLAGS="-O3 -fopenmp" CXXFLAGS="-O3 -fopenmp" ./configure
 sudo make install
-DEB=./deb
-rm -rf $DEB/usr/local
+
+DEB=`mktemp -d -p . surfer_deb.XXXXXXXXXX` || exit 1
+echo -e "\033[1mcreating .deb content in $DEB" && tput sgr0
+
+cp -r deb/* $DEB/
+find $DEB/ -type d -regex '.*/.svn$' | xargs rm -rf
+
 mkdir -p $DEB/usr/local/bin/
 mkdir -p $DEB/usr/local/share/surf/
-mkdir -p $DEB/usr/local/share/surfer/ 
+mkdir -p $DEB/usr/local/share/surfer/
+
+echo -e "\033[1mcopying surf and surfer binaries" && tput sgr0
 cp ./surf/surf $DEB/usr/local/bin/
 cp /usr/local/bin/surfer $DEB/usr/local/bin/
-cp -r /usr/local/share/surfer/ $DEB/usr/local/share/surfer/ 
-rm -rf $DEB/usr/local/share/surfer/gallery/gallery.tar
+
+echo -e "\033[1mcopying the gallery" && tput sgr0
+cp -r /usr/local/share/surfer/* $DEB/usr/local/share/surfer/ 
+rm -rf $DEB/usr/local/share/surfer/gallery.tar
+
+echo -e "\033[1mcopying locales" && tput sgr0
+LOCALE_FILES="/usr/local/share/locale/de/LC_MESSAGES/surfer.mo
+/usr/local/share/locale/en@quot/LC_MESSAGES/surfer.mo
+/usr/local/share/locale/en@boldquot/LC_MESSAGES/surfer.mo
+/usr/local/share/locale/en_GB/LC_MESSAGES/surfer.mo"
+for lf in $LOCALE_FILES
+do
+	mkdir -p `dirname $DEB$lf`
+	cp $lf $DEB$lf
+done
+
+echo -e "\033[1mcopying ui style files" && tput sgr0
 cp surfer_ui_exhibition.xml $DEB/usr/local/share/surfer/
-dpkg -b ./deb surfer.deb
+
+echo -e "\033[1mpackaging .deb" && tput sgr0
+dpkg -b $DEB surfer.deb
+
+echo -e "\033[1mremoving temporary folder $DEB" && tput sgr0
+rm -rf $DEB
